44da2f8e5d63130fba83e715ffb4b687a2addbc9
George Wan <singonwan@gmail.com>
2019-08-12 16:26:47 -0400

    learned nesting components from lesson 12

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
+import Ninjas from './Ninjas';
 
+//this is our ROOT COMPONENT that is rendered to the DOM
 class App extends Component {
   render() {
     return (
     <div className="App">
       <h1>My First ReactApp</h1>
       <p>Welcome!</p>
+      <Ninjas />
     </div>
     );
   }
