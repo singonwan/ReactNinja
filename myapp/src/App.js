ce997cf01e78fc1df5b4f58eca4865ed8b7d40e8
George Wan <singonwan@gmail.com>
2019-08-12 17:42:07 -0400

    finished lesson 14, learned how to output lists of objects, the js map func

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -6,13 +6,21 @@ import Ninjas from './Ninjas';
 //state stores local data for a component
 // but props can pass data from parent to child component. props must be strings?
 class App extends Component {
+  //each array item is an object 
+  //passing the array as a prop
+  state = {
+    ninjas :[ 
+      {name: 'Gwan', age: 26,  belt: 'Brack', id: 1 },
+      {name: 'Roni', age: 25,  belt: 'Blue', id: 2 },
+      {name: 'Ed', age: 25,  belt: 'Green', id: 3 }
+    ]
+  }
   render() {
     return (
     <div className="App">
       <h1>My First ReactApp</h1>
       <p>Welcome!</p>
-      <Ninjas name="Gwan" age="26" belt="Brack"/>
-      <Ninjas name="Roni" age="25" belt="Barack"/>
+      <Ninjas ninjas={this.state.ninjas} />
     </div>
     );
   }
--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -4,12 +4,23 @@ class Ninjas extends Component{
     render(){
         // console.log(this.props);
         //destructuring, var names must be the same as prop names
-        const { name, age, belt} = this.props;
+        // const { name, age, belt} = this.props;
+        const {ninjas} = this.props; //array is stored
+        //new array full of html elements
+        const ninjaList = ninjas.map(ninja => { //for each ninja we return the following JSX
+            return (
+                //Warning: Each child in a list should have a unique "key" prop. so react can identify which ninja this is for 
+                // easier removal or manipulation in the future
+            <div className="ninja" key={ ninja.id }>
+                <div>Name: {ninja.name}</div>
+                <div>Age: {ninja.age}</div>
+                <div>Belt: {ninja.belt}</div>
+            </div>
+            )
+        })
         return(
-            <div className="ninja">
-                <div>Name: {name}</div>
-                <div>Age: {age}</div>
-                <div>Belt: {belt}</div>
+            <div className='ninja-list'>
+                { ninjaList }
             </div>
         )
     }
