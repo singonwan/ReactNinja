b8e3e726604def0ebc8df386b0424d3ab925c977
George Wan <singonwan@gmail.com>
2019-08-13 18:20:16 -0400

    lesson 16 learned conditional output, using if/else or ternary operator- ternary is cleaner imo

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -3,22 +3,40 @@ import React from 'react';
 // class Ninjas extends Component{
 const Ninjas = ({ninjas}) => { //destruct in here instead of down there. 
     // render(){
-        // console.log(this.props);
         //destructuring, var names must be the same as prop names
         // const { name, age, belt} = this.props;
-    // const {ninjas} = props; //array is stored
+        // const {ninjas} = props; //array is stored
     //new array full of html elements
-    const ninjaList = ninjas.map(ninja => { //for each ninja we return the following JSX
-    return (
-            //Warning: Each child in a list should have a unique "key" prop. so react can identify which ninja this is for 
-            // easier removal or manipulation in the future
-        <div className="ninja" key={ ninja.id }>
-            <div>Name: {ninja.name}</div>
-            <div>Age: {ninja.age}</div>
-            <div>Belt: {ninja.belt}</div>
-        </div>
-        )
-    })
+    
+    // const ninjaList = ninjas.map(ninja => { //for each ninja we return the following JSX
+    //     //conditional output 
+    //     if (ninja.age > 25){
+    //         return (
+    //             //Warning: Each child in a list should have a unique "key" prop. so react can identify which ninja this is for 
+    //             // easier removal or manipulation in the future
+    //         <div className="ninja" key={ ninja.id }>
+    //             <div>Name: {ninja.name}</div>
+    //             <div>Age: {ninja.age}</div>
+    //             <div>Belt: {ninja.belt}</div>
+    //         </div>
+    //         )
+    //     } else {
+    //         return null
+    //     }
+    // })
+
+    //using js ternary condition operator       //condition ? ():()
+    //LOGIC UP HERE - SAVE IT IN A VARIABLE CONST
+    const ninjaList = ninjas.map(ninja => {
+        return ninja.age > 25 ? (
+                <div className="ninja" key={ ninja.id }>
+                    <div>Name: {ninja.name}</div>
+                    <div>Age: {ninja.age}</div>
+                    <div>Belt: {ninja.belt}</div>
+                </div>
+        ) : null
+    });
+    //ADD TO TEMPLATE TO RENDER
     return(
         <div className='ninja-list'>
             { ninjaList }
