0c41d4b12265efb8d59d6823000961d79bb5ec56
George Wan <singonwan@gmail.com>
2019-08-13 16:52:25 -0400

    changed Ninjas.js into a functional/stateless component instead of a container aka class component

--- myapp/src/Ninjas.js
+++ myapp/src/Ninjas.js
@@ -1,29 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-class Ninjas extends Component{
-    render(){
+// class Ninjas extends Component{
+const Ninjas = ({ninjas}) => { //destruct in here instead of down there. 
+    // render(){
         // console.log(this.props);
         //destructuring, var names must be the same as prop names
         // const { name, age, belt} = this.props;
-        const {ninjas} = this.props; //array is stored
-        //new array full of html elements
-        const ninjaList = ninjas.map(ninja => { //for each ninja we return the following JSX
-            return (
-                //Warning: Each child in a list should have a unique "key" prop. so react can identify which ninja this is for 
-                // easier removal or manipulation in the future
-            <div className="ninja" key={ ninja.id }>
-                <div>Name: {ninja.name}</div>
-                <div>Age: {ninja.age}</div>
-                <div>Belt: {ninja.belt}</div>
-            </div>
-            )
-        })
-        return(
-            <div className='ninja-list'>
-                { ninjaList }
-            </div>
+    // const {ninjas} = props; //array is stored
+    //new array full of html elements
+    const ninjaList = ninjas.map(ninja => { //for each ninja we return the following JSX
+    return (
+            //Warning: Each child in a list should have a unique "key" prop. so react can identify which ninja this is for 
+            // easier removal or manipulation in the future
+        <div className="ninja" key={ ninja.id }>
+            <div>Name: {ninja.name}</div>
+            <div>Age: {ninja.age}</div>
+            <div>Belt: {ninja.belt}</div>
+        </div>
         )
-    }
+    })
+    return(
+        <div className='ninja-list'>
+            { ninjaList }
+        </div>
+    )
 }
 
 export default Ninjas
