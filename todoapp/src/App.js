47b6393c260fa69b13605731235b4b1f157239d6
George Wan <singonwan@gmail.com>
2019-08-15 18:17:25 -0400

    added addform to todo app. finished part 2 as well

--- todoapp/src/App.js
+++ todoapp/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Todos from './Todos'
+import AddTodo from './AddForm'
 
 class App extends Component {
   state = {
@@ -9,6 +10,7 @@ class App extends Component {
     ]
   }
   deleteTodo = (id) => {
+    console.log(id)
     const todos = this.state.todos.filter(todo => {
       return todo.id !== id
     })
@@ -16,11 +18,19 @@ class App extends Component {
       todos: todos
     })
   }
+  addTodo = (todo) => {
+    todo.id = Math.random()
+    let todos = [...this.state.todos, todo]
+    this.setState({
+      todos
+    })
+  }
   render() {
     return (
       <div className="todo-app container">
         <h1 className="center blue-text">Todo's</h1>
         <Todos todos={this.state.todos} deleteTodo={this.deleteTodo}/>
+        <AddTodo addTodo={this.addTodo}/>
       </div>
     );
   }
