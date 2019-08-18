cf70e2e8a5f89f83a27284cccfc634930efecf44
George Wan <singonwan@gmail.com>
2019-08-17 22:19:06 -0400

    added comments about props

--- reactrouterapp/src/hoc/Rainbow.js
+++ reactrouterapp/src/hoc/Rainbow.js
@@ -3,15 +3,14 @@ import React from 'react'
 const Rainbow = (WrappedComponent) => {
 
     const colours = ['red', 'pink', 'orange', 'yellow', 'green', 'blue', 'purple'];
-    const randomColour = colours[Math.floor(Math.random() * 7)];
+    const randomColour = colours[Math.floor(Math.random() * colours.length)];
     const className = randomColour + '-text';
-
-    return (props) => {
+    
+    return (props) => { //props passed back into wrapped component?
         return (
             <div className={className}>
                 <WrappedComponent {...props} />
             </div>
-
         )
     }
 }
