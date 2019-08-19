473fb7ae03ae90fd7efca826c27d6ac9cc466d11
George Wan <singonwan@gmail.com>
2019-08-18 22:13:02 -0400

    lesson 30, use,set and grab route parameters after componentdidmount in a component

--- reactrouterapp/src/App.js
+++ reactrouterapp/src/App.js
@@ -4,16 +4,19 @@ import { BrowserRouter, Route } from 'react-router-dom'
 import Home from './components/Home'
 import About from './components/About'
 import Contact  from './components/Contact'
+import Post from './components/Post'
 
 function App() {
   return (
     //Route, whenever path is entered go to component
+    //route parameter //:post_id
     <BrowserRouter>
       <div className="App">
         <Navbar />
         <Route exact path='/' component={Home} />
         <Route path='/about' component={About} />
         <Route path='/contact' component={Contact} />
+        <Route path='/:post_id' component={Post} /> 
       </div>
     </BrowserRouter>
   );
