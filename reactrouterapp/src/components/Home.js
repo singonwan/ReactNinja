c91f3f05d2a3710089746d94572b88f1892ffedd
George Wan <singonwan@gmail.com>
2019-08-18 22:28:05 -0400

    lesson 31, complete route parameters, managed to show title and body

--- reactrouterapp/src/components/Home.js
+++ reactrouterapp/src/components/Home.js
@@ -1,5 +1,6 @@
-import React, { Component} from 'react'
+import React, { Component } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 class Home extends Component {
     state = {
@@ -8,7 +9,7 @@ class Home extends Component {
     componentDidMount(){
         axios.get('https://jsonplaceholder.typicode.com/posts') //async. returns promise. can use .then
             .then(res => {
-                console.log(res)
+                // console.log(res)
                 this.setState({
                     posts: res.data.slice(0,10)
                 })
@@ -21,7 +22,9 @@ class Home extends Component {
                 return(
                     <div className="post card" key={post.id}>
                         <div className="card-content">
+                            <Link to={'/' + post.id}>
                             <span className="card-title">{post.title}</span>
+                            </Link>
                             <p>{post.body}</p>
                         </div>
                     </div>
